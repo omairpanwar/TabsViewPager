16a4b8e05d3651d08e788b0cfae89a6aefe163a7
hussnain <13014198-088@uog.edu.pk>
2018-05-18 20:04:54 +0500

    alert dialog added done.

--- app/src/main/java/com/umairpanwar/tabsviewpager/fragments/FragmentTwo.java
+++ app/src/main/java/com/umairpanwar/tabsviewpager/fragments/FragmentTwo.java
@@ -1,21 +1,26 @@
 package com.umairpanwar.tabsviewpager.fragments;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.umairpanwar.tabsviewpager.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentTwo extends Fragment {
-
+    @BindView(R.id.button)
+    Button btn;
 
     public FragmentTwo() {
         // Required empty public constructor
@@ -29,6 +34,17 @@ public class FragmentTwo extends Fragment {
 
         View view =  inflater.inflate(R.layout.fragment_two, container, false);
         ButterKnife.bind(this, view);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setMessage("hello world pakistan");
+                builder.setPositiveButton("OK",null);
+                builder.setNegativeButton("Cancel",null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
         return view;
     }
 
